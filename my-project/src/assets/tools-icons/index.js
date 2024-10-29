5ab8500e66467822ef8f66bcfa750254fc569c41
rosachen3 <rosa.chen3@hotmail.com>
2024-10-28 23:30:47 -0400

    feat: tools in my skills section

---
+++ my-project/src/assets/tools-icons/index.js
@@ -0,0 +1,12 @@
+import canva from "./canva.png"
+import figma from "./figma.png"
+import git from "./git.png"
+import github from "./github.png"
+import intellij from "./intellij.png"
+import jupyter from "./jupyter.png"
+import matlab from "./matlab.png"
+import vscode from "./vscode.png"
+
+export default {
+    canva, figma, git, github, intellij, jupyter, matlab, vscode
+}
--- my-project/src/components/my skills/MySkills.js
+++ my-project/src/components/my skills/MySkills.js
@@ -1,18 +1,21 @@
 import Button from "../Button.js"
 import logos from "../../assets/techstack-icons"
+import tools from "../../assets/tools-icons"
 
 export default function MySkills() {
 
     const { c, css, html, java, js, python, reactjs } = logos
+    const { canva, figma, git, github, intellij, jupyter, matlab, vscode } = tools
 
     return (
         <div className="flex flex-col p-14 items-center p-20">
             <h2 className="mr-auto">My Skills</h2>
-            <p className="mr-auto mb-8">Here are some of the skills I am working on</p>
+            <p className="mr-auto my-8">Here are some of the skills I am working on</p>
             {/*Tech Stack */}
             
             <div className="flex flex-col gap-8 bg-white p-12 rounded-2xl items-center w-3/4">
                 <div className="flex gap-6">
+                    {/*Row 1*/}
                     {
                         Object.entries(logos).slice(0,4).map(logo => {
                             const [name, icon] = logo
@@ -21,6 +24,7 @@ export default function MySkills() {
                     }
                 </div>
                 <div className="flex gap-6">
+                    {/*Row 2*/}
                     {
                         Object.entries(logos).slice(4,7).map(logo => {
                             const [name, icon] = logo
@@ -30,9 +34,25 @@ export default function MySkills() {
                 </div>
             </div>
             
-
-            <div className="bg-white">
-                
+            {/*Tools */}
+            <p className="mr-auto my-8">And these are some of the tools I have been working with:</p>
+            <div className="flex flex-col gap-8 bg-white p-12 rounded-2xl items-center w-3/4 mt-4">
+                <div className="flex gap-6"> 
+                    {   
+                        Object.entries(tools).slice(0,4).map(tool => {
+                            const [name, icon] = tool
+                            return <Button text={name} iconLeft={icon}/>
+                        })
+                    }
+                </div>
+                <div className="flex gap-6">
+                    {
+                        Object.entries(tools).slice(4,8).map(tool => {
+                            const [name, icon] = tool
+                            return <Button text={name} iconLeft={icon}/>
+                        })
+                    }
+                </div>
             </div>
         </div>
     )
